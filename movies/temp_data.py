9036372c88a30c0f4cd2047975313fb5d1db4c67
Maria <mfpcortez@usp.br>
2023-11-15 17:53:59 -0300

    Inserir listagem, busca e criação de filmes

---
+++ movies/temp_data.py
@@ -0,0 +1,56 @@
+movie_data = [{
+    "id":
+    "1",
+    "name":
+    "The Shawshank Redemption",
+    "release_year":
+    "1994",
+    "poster_url":
+    "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
+}, {
+    "id":
+    "2",
+    "name":
+    "The Godfather",
+    "release_year":
+    "1972",
+    "poster_url":
+    "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
+}, {
+    "id":
+    "3",
+    "name":
+    "Schindler's List",
+    "release_year":
+    "1993",
+    "poster_url":
+    "https://image.tmdb.org/t/p/w500/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg"
+}, {
+    "id":
+    "4",
+    "name":
+    "Black Beauty",
+    "release_year":
+    "2020",
+    "poster_url":
+    "https://image.tmdb.org/t/p/w500/5ZjMNJJabwBEnGVQoR2yoMEL9um.jpg"
+}, {
+    "id":
+    "5",
+    "name":
+    "The Godfather: Part II",
+    "release_year":
+    "1974",
+    "poster_url":
+    "https://image.tmdb.org/t/p/w500/hek3koDUyRQk7FIhPXsa6mT2Zc3.jpg"
+}, {
+    "id":
+    "6",
+    "name":
+    "Your Name.",
+    "release_year":
+    "2016",
+    "poster_url":
+    "https://image.tmdb.org/t/p/w500/q719jXXEzOoYaps6babgKnONONX.jpg"
+}
+]
